137d7eae4de389ec89bae5c7315c911cfb49b9cf
Shaun Fuller <fullers@email.unc.edu>
2016-08-03 23:00:30 -0400

    added images still need to finish js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,75 @@
 // Javascript to control the actions of the hangman game
 
 var wordsArray = ["Alf", "Hangman", "Divergent", "Insurgent", "Allegiant", "Aladdin", "Terminator"];
+//var wordsArray = ["monitor", "program", "application", "keyboard", "javascript", "gaming", "network"];
 var word;
 var goesLeft;
 var placeholder;
 var input;
 var wordLength;
+var wordSubstring;
 var currentWord;
-var guessedLetter;
-var tries;
 var inputArray = [];
+var numWins;
 
-  function newGame(){
-     placeholder = "";
-     goesLeft = 10;
-     word = wordsArray[getRandom(0,wordsArray.length)];
-     wordLength = word.length;
-     console.log(word);
-     
-     var myPicElement = document.getElementById("hangimage").src = "http://fetlar.kingston.ac.uk/pp/hangman10.jpg";
+var numGuessRemain = document.getElementById("numGuessRemain");
 
-     for (var count = 0; count < word.length; count++)
-     {
-          placeholder = placeholder + " _";
-     }
+  function newGame()
+  {
 
-     document.getElementById("placeholder").innerHTML = placeholder;
-     // document.getElementById("gamestatus").innerHTML = "Game running";
+    placeholder = "";
+    goesLeft = 10;
+    numWins = 0;
+    word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    currentWord = 0;
+    word = wordsArray[currentWord];
+    wordLength = word.length;
+    wordSubstring = currentWord.substring;
+    console.log(word);
+    //var myElement = document.getElementById("button").innerHTML = "Click to guess";
+    var myPicElement = document.getElementById("hangimage").src = "http://fetlar.kingston.ac.uk/pp/hangman10.jpg";
+
+    for (var count = 0; count < word.length; count++)
+    {
+      placeholder = placeholder + " _";
+    }
+
+    document.getElementById("placeholder").innerHTML = placeholder;
+    //document.getElementById("gamestatus").innerHTML = "Game running";
+
+    numGuessRemain.innerHTML = goesLeft;
+    wins.innerHTML = numWins;
   }
-  
-  function getRandom(min,max){
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-  
+
   function guessLetter()
   {
-     var correct = 0;
+    var correct = 0;
 
-     for (var count = 0; count < wordLength; count++)
-     {
-       if (input == word.substring(count, count + 1))
-       {
+    // var inputBox = document.getElementById("guessinput");
+    // input = inputBox.value;
+
+
+    for (var count = 0; count < wordLength; count++)
+    {
+      if (input == word.substring(count, count + 1))
+      {
+         
         correct++;
         placeholder = placeholder.substring(0, count) + input + placeholder.substring(count + 1, placeholder.length + 1);
         document.getElementById("placeholder").innerHTML = placeholder;
-       }
+      }
     }
 
     if (correct == 0)
     {
       goesLeft--;
     }
-    var url = document.getElementById("hangimage").src = "http://fetlar.kingston.ac.uk/pp/hangman" + goesLeft + ".jpg";
+    var url = document.getElementById("hangimage").src = "assets/images/hangman" + goesLeft + ".png";
 
     if (placeholder == word)
     {
       document.getElementById("hangimage").src = "http://fetlar.kingston.ac.uk/pp/hangman_win.jpg";
+      numWins++
       alert("You guessed the word correctly. You win!");
     }
 
@@ -65,33 +79,24 @@ var inputArray = [];
       newGame();
     }
   }
+//newGame();
+//document.getElementById("button").onclick = guessLetter;
 
-  function nmbrGuesses() {
-  	tries = "12";
-
-  }
-
-  function usedLetters() {
-  	var status = document.getElementById('status');
-	for (var i=0; i <= 12; i++) {
-		status.innerHTML =  inputArray[i] + " "; 
-	}
-	
-  }
-
-
-// Calling Functions
 document.onkeyup = function(event) {
 
-	var input = String.fromCharCode(event.keyCode).toLowerCase();
-	usedLetters();
-	newGame();
+  
+ input = String.fromCharCode(event.keyCode).toLowerCase();
+ var status = document.getElementById('status');
+    
+  
+     for (var i=0; i < inputArray.length; i++) {
+        inputArray.push(input);
 
-	
+   status.innerHTML = input + " "; 
+ }
+  guessLetter();
+  newGame();
 }
-
-
-	
 
 
 
